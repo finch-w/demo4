b4948a60f3151e01add8ca5421461fd00c3016bd
yuan50697105 <710575900@qq.com>
2019-01-30 16:05:54 +0800

    null update

--- demo-jooq-jpa/src/test/java/com/yuan/demojooqjpa/DemoJooqJpaApplicationTests.java
+++ demo-jooq-jpa/src/test/java/com/yuan/demojooqjpa/DemoJooqJpaApplicationTests.java
@@ -1,38 +1,22 @@
 package com.yuan.demojooqjpa;
 
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.ResultQuery;
-import org.jooq.impl.DSL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.persistence.EntityManager;
-import java.util.Arrays;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoJooqJpaApplicationTests {
     @Autowired
-    private EntityManager entityManager;
-    @Autowired
-    private DSLContext dslContext;
+    private ApplicationContext context;
 
     @Test
     public void contextLoads() {
-        ResultQuery<Record> records = dslContext.resultQuery("select * from user u where u.id in (?)", DSL.sql("select * from user u"));
-        System.out.println(records.getSQL());
-        System.out.println(records.getResult());
     }
 
-    public void test() {
-        int[][] ints = {{1, 2}, {2, 3}};
-
-        Arrays.stream(ints).flatMap(ints1 -> Arrays.stream(ints)).forEach(System.out::println);
-    }
 
 }
 
--- demo-jpa2/src/test/java/com/yuan/demojpa2/DemoJpa2ApplicationTests.java
+++ demo-jpa2/src/test/java/com/yuan/demojpa2/DemoJpa2ApplicationTests.java
@@ -1,40 +1,20 @@
 package com.yuan.demojpa2;
 
-import org.hibernate.query.internal.QueryImpl;
-import org.hibernate.transform.Transformers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoJpa2ApplicationTests {
-
-    @Autowired
-    private EntityManager entityManager;
-
-    @Test
-    public void contextLoads() {
-    }
-
     @Autowired
     private ApplicationContext context;
 
-
     @Test
-    @Transactional
-    public void test2() {
-        EntityManager entityManager = context.getBean(EntityManager.class);
-        Query user = entityManager.createQuery("select u.id from User u");
-        user.unwrap(QueryImpl.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
-        System.out.println(user.getResultList());
+    public void contextLoads() {
     }
 
 
