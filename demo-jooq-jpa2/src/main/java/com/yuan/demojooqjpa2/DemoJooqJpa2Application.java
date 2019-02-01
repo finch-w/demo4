4e8abaf7d9490f4fbac3ec52e06e756e1aeb564a
710575900@qq.com <710575900@qq.com>
2019-02-01 21:55:56 +0800

    修复警告

--- demo-jooq-jpa2/src/main/java/com/yuan/demojooqjpa2/DemoJooqJpa2Application.java
+++ demo-jooq-jpa2/src/main/java/com/yuan/demojooqjpa2/DemoJooqJpa2Application.java
@@ -8,11 +8,13 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication(exclude = EasyPoiAutoConfiguration.class)
-@EnableCaching(proxyTargetClass = true)
-@EnableAsync(proxyTargetClass = true)
+@EnableCaching
+@EnableAsync
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(proxyTargetClass = true, jsr250Enabled = true, prePostEnabled = true, securedEnabled = true)
 @EnableSpringHttpSession
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
--- demo-mybatis2/src/main/java/com/yuan/demomybatis2/DemoMybatis2Application.java
+++ demo-mybatis2/src/main/java/com/yuan/demomybatis2/DemoMybatis2Application.java
@@ -4,14 +4,19 @@ import cn.afterturn.easypoi.configuration.EasyPoiAutoConfiguration;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication(exclude = EasyPoiAutoConfiguration.class)
-//@EnableCaching
+@EnableCaching
 @EnableAsync
-//@EnableGlobalMethodSecurity(proxyTargetClass = true, jsr250Enabled = true, prePostEnabled = true, securedEnabled = true)
-//@EnableSpringHttpSession
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(proxyTargetClass = true, jsr250Enabled = true, prePostEnabled = true, securedEnabled = true)
+@EnableSpringHttpSession
 public class DemoMybatis2Application {
 
     public static void main(String[] args) {
