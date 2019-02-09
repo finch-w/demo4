ac9abe0ba3345d89a5f0382bb1e910b147a4aea4
710575900@qq.com <710575900@qq.com>
2019-02-09 23:57:10 +0800

    mybatis2 user role authority resource mybatis user role authority resource jpa user role add mybatis3 but it is a test project
    删除mybatis 3 与 Mybatis 4
    Mybatis Enhance不完善
    fastMybatis 功能不完善
    综合对比不如
    mapper+pagehelper与Mybatis plus

--- demo-test/src/test/java/com/yuan/demotest/DemoTestApplicationTests.java
+++ demo-test/src/test/java/com/yuan/demotest/DemoTestApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Objects;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoTestApplicationTests {
 
     @Test
     public void contextLoads() {
+        Objects.nonNull("aaa");
     }
 
 }
